9a7ea6786e60820367d917ae0f3302342f4cfce3
cpflayan <cpflayan@gmail.com>
2023-10-03 16:30:01 +0800

    ururi

--- bot-relas.py
+++ bot-relas.py
@@ -44,8 +44,8 @@ def add(update: Update, context: CallbackContext) -> int:
         now = datetime.now()
         currentDate = now.strftime("%Y-%m-%d")
         currentTime = now.strftime("%H:%M:%S")
-        current_month_start = datetime(now.year, now.month, 1)
-        current_month_end = datetime(now.year, now.month, calendar.monthrange(now.year, now.month)[1])
+        current_month_start = date(now.year, now.month, 1)
+        current_month_end = date(now.year, now.month, calendar.monthrange(now.year, now.month)[1])
         today = date.today().strftime("%Y-%m-%d")
 
         chat_id = update.message.chat.id
@@ -96,8 +96,8 @@ def subtract(update: Update, context: CallbackContext) -> int:
         now = datetime.now()
         currentDate = now.strftime("%Y-%m-%d")
         currentTime = now.strftime("%H:%M:%S")
-        current_month_start = datetime(now.year, now.month, 1)
-        current_month_end = datetime(now.year, now.month, calendar.monthrange(now.year, now.month)[1])
+        current_month_start = date(now.year, now.month, 1)
+        current_month_end = date(now.year, now.month, calendar.monthrange(now.year, now.month)[1])
         today = date.today().strftime("%Y-%m-%d")
 
         chat_id = update.message.chat.id
@@ -150,8 +150,8 @@ def add_fee(update: Update, context: CallbackContext) -> int:
         now = datetime.now()
         currentDate = now.strftime("%Y-%m-%d")
         currentTime = now.strftime("%H:%M:%S")
-        current_month_start = datetime(now.year, now.month, 1)
-        current_month_end = datetime(now.year, now.month, calendar.monthrange(now.year, now.month)[1])
+        current_month_start = date(now.year, now.month, 1)
+        current_month_end = date(now.year, now.month, calendar.monthrange(now.year, now.month)[1])
         today = date.today().strftime("%Y-%m-%d")
 
         chat_id = update.message.chat.id
@@ -222,8 +222,8 @@ def calculate_balance(update: Update, context: CallbackContext) -> int:
         year = int(match.group(1))
         month = int(match.group(2))
         # 計算指定月份的開始和结束日期
-        start_date = datetime(year, month, 1)
-        end_date = datetime(year, month, calendar.monthrange(year, month)[1])
+        start_date = date(year, month, 1)
+        end_date = date(year, month, calendar.monthrange(year, month)[1])
         start_date_formatted = start_date.strftime("%Y-%m").zfill(7)
         # 查詢指定月份的入金總額
         cursor.execute('SELECT SUM(amount) FROM transactions WHERE action="add" AND chat_id = ? AND date BETWEEN ? AND ?', (chat_id, start_date, end_date))
@@ -273,8 +273,8 @@ def calculate_balance(update: Update, context: CallbackContext) -> int:
         year = int(match.group(1))
         month = int(match.group(2))
         # 計算指定月份的開始和结束日期
-        start_date = datetime(year, month, 1)
-        end_date = datetime(year, month, calendar.monthrange(year, month)[1])
+        start_date = date(year, month, 1)
+        end_date = date(year, month, calendar.monthrange(year, month)[1])
         start_date_formatted = start_date.strftime("%Y-%m").zfill(7)
 
         # 查詢指定月份的入金總額
@@ -421,7 +421,7 @@ def export_to_excel(update: Update, context: CallbackContext) -> int:
     file_name = f" {chat_title}-{year_month}.xlsx"  # 替换群组名稱为实际的群组名
 
     # 查询指定月份的数据
-    cursor.execute('SELECT date, time, note, amount FROM transactions WHERE date LIKE ? AND chat_id = ?', (f'{year_month}%', chat_id))
+    cursor.execute('SELECT date, time, action, amount FROM transactions WHERE date LIKE ? AND chat_id = ?', (f'{year_month}%', chat_id))
     data = cursor.fetchall()
 
     if not data:
@@ -451,9 +451,9 @@ def show(update: Update, context: CallbackContext) -> int:
         currentDate = now.strftime("%Y-%m-%d")
         currentTime = now.strftime("%H:%M:%S")
 
-        current_month_start = datetime(now.year, now.month, 1)
+        current_month_start = date(now.year, now.month, 1)
 
-        current_month_end = datetime(now.year, now.month, calendar.monthrange(now.year, now.month)[1])
+        current_month_end = date(now.year, now.month, calendar.monthrange(now.year, now.month)[1])
 
         today = date.today().strftime("%Y-%m-%d")
         cursor.execute('SELECT COUNT(*) FROM transactions WHERE action="add" AND chat_id = ? AND date BETWEEN ? AND ?', (chat_id, current_month_start, current_month_end))
@@ -488,7 +488,7 @@ def show(update: Update, context: CallbackContext) -> int:
         last_month_count = cursor.fetchone()[0] or 0
 
         total = add_total - subtract_total - fee_total + last_month_count
-        context.bot.send_message(chat_id=chat_id, text=f"({currentDate} {currentTime} \n\n------------------------------------------------\n本日:\n入{add_today_count}筆:{add_today_total},出{subtract_today_count}筆:{subtract_today_total}\n本月:\n入{add_count}筆:{add_total},出{subtract_count}筆:{subtract_total}\n手續費月計:{fee_total},前期結餘:{last_month_count}\n總計:{total}\n(月計-月計手續費+前期結餘)")
+        context.bot.send_message(chat_id=chat_id, text=f"{currentDate} {currentTime} \n\n------------------------------------------------\n本日:\n入{add_today_count}筆:{add_today_total},出{subtract_today_count}筆:{subtract_today_total}\n本月:\n入{add_count}筆:{add_total},出{subtract_count}筆:{subtract_total}\n手續費月計:{fee_total},前期結餘:{last_month_count}\n總計:{total}\n(月計-月計手續費+前期結餘)")
         
 def list_daily_flow(update: Update, context: CallbackContext) -> int:
     chat_id = update.message.chat.id
